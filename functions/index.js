6a5f8e89fbd89c8b79c187044aa3e1636a6e14e2
Ahmad Nizar <ahmad.rpl001@gmail.com>
2021-06-09 21:52:05 +0700

    install babel-eslint

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,4 @@
+const userController = require("./app/users/router");
 const {functions} = require("./app/config");
 const express = require("express");
 const app = express();
@@ -5,9 +6,7 @@ const app = express();
 const cors = require("cors");
 app.use(cors({origin: true}));
 
-app.get("/api/test/", (req, res) =>{
-  return res.status(200).send("Hello world");
-});
+app.use("/api", userController);
 
 // Export the api to firebase cloud functions
 exports.app = functions.region("asia-southeast2").https.onRequest(app);
